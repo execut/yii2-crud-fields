9f75843b385c35f4a359dfc0215cb066ba61fc63
eXeCUT <sexecut@gmail.com>
2018-03-29 16:19:52 +0300

    Fixed crud fields

--- Relation.php
+++ Relation.php
@@ -268,7 +268,7 @@ class Relation extends BaseObject
                 $url['id'] = $row->$attribute;
             }
 
-            return Html::a($value . '&nbsp;>>>', Url::to($url), ['title' => $this->field->getLabel() . ' - перейти к редактированию', 'draggable' => 'false']);
+            return $value . '&nbsp;' . Html::a('>>>', Url::to($url), ['title' => $this->field->getLabel() . ' - перейти к редактированию']);
         } else {
             $models = $row->{$this->getName()};
             $result = '';
@@ -289,7 +289,7 @@ class Relation extends BaseObject
 
                 $url = [$url . '/update', 'id' => $model->primaryKey];
 
-                $result .= $value . '&nbsp;' . Html::a('>>>', Url::to($url), ['title' => $this->field->getLabel() . ' - перейти к редактированию', 'draggable' => 'false']) . '<br>';
+                $result .= $value . '&nbsp;' . Html::a('>>>', Url::to($url), ['title' => $this->field->getLabel() . ' - перейти к редактированию']) . '<br>';
             }
 
             return $result;
