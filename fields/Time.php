b8e1baa74151ca4db2684d552ad1be5332e12675
eXeCUT notebook <sexecut@gmail.com>
2019-12-25 13:31:50 +0300

    fix

--- fields/Time.php
+++ fields/Time.php
@@ -18,6 +18,7 @@ class Time extends Date
 {
     public $fieldType = DetailView::INPUT_TIME;
     public $multipleInputType = DetailView::INPUT_TIME;
+    public $isTime = true;
     public function getWidgetOptions(): array
     {
         return ArrayHelper::merge(parent::getWidgetOptions(), [
