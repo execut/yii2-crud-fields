5823000607e0949c154fbcfd337432ac0f8f2854
eXeCUT <sexecut@gmail.com>
2020-02-12 14:55:15 +0300

    Added depend for asset

--- widgets/Select2ExecutAsset.php
+++ widgets/Select2ExecutAsset.php
@@ -5,7 +5,11 @@ namespace execut\crudFields\widgets;
 
 
 use execut\yii\web\AssetBundle;
+use yii\jui\JuiAsset;
 
 class Select2ExecutAsset extends AssetBundle
 {
+    public $depends = [
+        JuiAsset::class,
+    ];
 }
