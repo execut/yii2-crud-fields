8773e1e7030581fbd114af6dcb7f7b0165750062
eXeCUT <sexecut@gmail.com>
2020-02-21 09:38:21 +0300

    added uncomplete test for has many

---
+++ tests/unit/fields/HasManyMultipleInputTest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace execut\crudFields\fields;
+
+
+use execut\crudFields\TestCase;
+
+class HasManyMultipleInputTest extends TestCase
+{
+    public function testGetColumn() {
+        $this->markTestIncomplete('later');
+        return;
+        $field = new HasManyMultipleInput();
+        $column = $field->getColumn();
+        $this->assertArrayHasKey('value', $column);
+    }
+}
